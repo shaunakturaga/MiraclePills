33977fa032e8789fe200b76249adb3c753446260
Shaunak turaga <shaunakturaga@gmail.com>
2016-12-29 13:54:48 -0500

    Initial Commit

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Shaunak Turaga on 12/29/16.
+//  Copyright © 2016 Shaunak Turaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
